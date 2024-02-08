d7f56fc5ca9df7ba83383a778a296092c2b4765f
zzx <93886174@qq.com>
2024-02-08 17:39:49 +0800

    style: 将属性其改为不可变类型，取消主构造函数代码

--- Src/Bika.Downloader.Core/Model/DownloadedComic.cs
+++ Src/Bika.Downloader.Core/Model/DownloadedComic.cs
@@ -1,14 +1,14 @@
 namespace Bika.Downloader.Core.Model;
 
-public record DownloadedComic()
+public record DownloadedComic
 {
-    public string Id { get; set; }
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public bool Finished { get; set; }
-    public List<string> EpisodesId { get; set; }
+    public string Id { get; init; }
+    public string Title { get; init; }
+    public string Author { get; init; }
+    public bool Finished { get; init; }
+    public List<string> EpisodesId { get; init; }
 
-    public DownloadedComic(Comic comic) : this()
+    public DownloadedComic(Comic comic)
     {
         Id = comic.Id;
         Title = comic.Title;
